d6c30bc0d79af67c67bbb2e9e7365bce316f0b83
Carol St.Louis <stlouis_c@yahoo.com>
2016-04-18 22:44:55 -0700

    added general game specs

---
+++ imports/client/constants/actionTypes.js
@@ -0,0 +1,55 @@
+const gameStats = {
+  EASY: {
+    label: "easy",
+    value: "DIFFICULTY_EASY"
+  },
+  NORMAL: {
+    label: "normal",
+    value: "DIFFICULTY_NORMAL"
+  },
+  HARD: {
+    label: "hard",
+    value: "DIFFICULTY_HARD"
+  }
+};
+
+const gameStatus = {
+  PLAYING: {
+    label: "playing",
+    value: "GAME_STATUS_PLAYING"
+  },
+  OVER: {
+    label: "gameover",
+    value: "GAME_STATUS_GAMEOVER"
+  },
+  CLEARED: {
+    label: "cleared",
+    value: "GAME_STATUS_CLEARED"
+  }
+};
+
+const flagActions = {
+  UPDATE: "UPDATE_FLAGS",
+  RESET: "RESET_FLAGS"
+};
+
+const openTiles = {
+  UPDATE: "UPDATE_OPEN_TILES",
+  RESET: "RESET_OPEN_TILES"
+};
+
+const gameTime = {
+  UPDATE: "UPDATE_TIME",
+  RESET: "RESET_TIME"
+};
+
+// root action types
+const actionTypes = {
+  gameStats,
+  gameStatus,
+  flagActions,
+  openTiles,
+  gameTime
+};
+
+export default actionTypes;
---
+++ imports/client/reducers/flagCounts.js
@@ -0,0 +1,14 @@
+import { flagActions } from 'imports/client/constants/actionTypes.js';
+const { UPDATE, RESET } = flagActions;
+const INITIAL_FLAG_COUNT = 0;
+
+export default function flagNumbers(state = INITIAL_FLAG_COUNT, action){
+	switch(action.type){
+		case UPDATE:
+			return state + action.data;
+		case RESET: 
+			return INITIAL_FLAG_COUNT;
+		default:
+			return state;
+	}
+}
